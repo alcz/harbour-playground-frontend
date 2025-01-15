70a9900f9638bc65ce73fbfbd2cf21129eac88be
Aleksander Czajczynski <hb@fki.pl>
2025-01-16 00:56:30 +0100

    added crunner.html and friends, it is a variant of Harbour Playground that integates a WebAssembly runner with a GUI, in the same tab browser. Also needs runner.wasm, runner.js from another repo placed in static/, see https://github.com/alcz/harbour-cimgui-sokol-starterkit/blob/master/examples/runner.prg - I will add a binary to this repo after it settles a bit.

--- static/concat.sh
+++ static/concat.sh
@@ -1,2 +1,2 @@
 cat codemirror.js codemirror-hb.js hbio/hbio.js hbio/bin.js hbio/utf8.js playground.js playground-embed.js > playground-full.js
-cat style.css codemirror.css codemirror-base16-light.css codemirror-material.css codemirror-monokai.css > playground-full.css
+cat style.css runner.css codemirror.css codemirror-base16-light.css codemirror-material.css codemirror-monokai.css > playground-full.css
